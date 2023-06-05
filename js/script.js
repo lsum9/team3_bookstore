f7222e87c40583897c80824a681088ebd15c3f2a
woolen-h <wobada@naver.com>
2023-06-05 15:04:20 +0900

    [CSS/JS] 캐러셀 적용

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$('.slide-1').on('click', function() {
+  $('.slide-container').css('transform', 'translateX(0px)');
+});
+
+$('.slide-2').on('click', function() {
+  $('.slide-container').css('transform', 'translateX(-300px)');
+});
+
+$('.slide-3').on('click', function() {
+  $('.slide-container').css('transform', 'translateX(-600px)');
+});
