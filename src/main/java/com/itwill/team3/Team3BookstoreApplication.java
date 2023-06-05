20359998421aa2cdcc4e9cf64c4837e7195817b5
woolen-h <wobada@naver.com>
2023-06-05 22:30:46 +0900

    [Form] 스프링 스타터 프로젝트로 업데이트

---
+++ src/main/java/com/itwill/team3/Team3BookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.itwill.team3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team3BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team3BookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itwill/team3/Team3BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itwill.team3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team3BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
